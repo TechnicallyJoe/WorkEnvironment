e83a0139038298484b358f43f8084cdbfa0ec55e
Jonathan Østrup <jro@pentia.dk>
2020-03-13 11:25:29 +0100

    Added run.ps1 - Main entry point.
    Added entities.yml - Has a list of packages from chocolatey.

---
+++ run.ps1
@@ -0,0 +1,40 @@
+[cmdletBinding()]
+
+param
+(
+    # Packages YAML file path
+    [Parameter()]
+    [string] $PackagesPath = "$PSScriptRoot/entities.yml"
+)
+
+Write-Verbose 'loading dependencies'
+foreach ($item in (Get-ChildItem -Path "$PSScriptRoot/src/" -Recurse -File))
+{
+    Write-Verbose "Loading: $($item.Name)"
+    . $item.FullName
+}
+
+Write-Verbose "Confirming privileged mode is on."
+if (-not(Confirm-Administrator)) { break; }
+
+Write-Verbose 'Confirming Chocolatey Is installed.'
+if (-not(Confirm-Chocolatey)) { Install-Chocolatey }
+
+Write-Verbose "Loading modules"
+if (-not(Get-Module -ListAvailable 'powershell-yaml'))
+{
+    Install-Module -Name 'powershell-yaml'
+}
+else
+{
+    Import-Module -Name 'powershell-yaml'
+}
+
+Write-Verbose "Loading Scripts"
+$packages = Get-Content -Path $PackagesPath -Raw | ConvertFrom-Yaml
+$packages.packages
+
+foreach ($package in $packages.packages)
+{
+    choco upgrade -y $package
+}
