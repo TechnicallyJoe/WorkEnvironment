7ea8e970ce82c2b6327ca5167d4f329338a83cf9
Jonathan Østrup <jesusravn@gmail.com>
2020-03-18 14:49:45 +0100

    refactored ps-profile task

--- components/ps-profile/task.ps1
+++ components/ps-profile/task.ps1
@@ -7,19 +7,27 @@ param
 
     # path to the src folder
     [Parameter()]
-    [string] $srcPath = "$PSScriptRoot\src"
-)
+    [string] $srcPath = "$PSScriptRoot\src",
 
-$profileParent = (Split-Path -Path $Profile -Parent)
-$profileName = (Split-Path -Path $Profile -leaf)
-$profileTemplateName = "profile.ps1"
+    # Name of the Profile file.
+    [Parameter()]
+    [string] $ProfileName = (Split-Path -Path $Profile -leaf),
+
+    # Profile root path
+    [Parameter()]
+    [string] $ProfilePath = (Split-Path -Path $Profile -Parent),
+
+    # Profile template name
+    [Parameter()]
+    [string] $ProfileTemplateName = 'profile.ps1'
+)
 
 if (-not(Test-Path -Path $profile)) 
 {
-    if (-not(Test-Path -Path $profileParent)) { New-Item -Path $profileParent -Type Directory }
+    if (-not(Test-Path -Path $ProfilePath)) { New-Item -Path $ProfilePath -Type Directory }
 
-    Copy-Item -Path "$srcPath\$profileTemplateName" -Destination $profileParent -Force
-    Rename-Item -Path "$profileParent\$profileTemplateName" -NewName $profileName -Force
+    Copy-Item -Path "$srcPath\$profileTemplateName" -Destination $ProfilePath -Force
+    Rename-Item -Path "$ProfilePath\$profileTemplateName" -NewName $ProfileName -Force
 }
 else
 {
