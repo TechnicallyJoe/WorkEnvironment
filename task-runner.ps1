418c8ae881ba918468948f26bc0fb9243611d76f
Jonathan Østrup <jesusravn@gmail.com>
2020-03-18 14:49:17 +0100

    refactored task-runner.ps1

--- task-runner.ps1
+++ task-runner.ps1
@@ -1,42 +1,14 @@
-[cmdletBinding()]
-
-param
-(
-    # Packages YAML file path
-    [Parameter()]
-    [string] $PackagesPath = "$PSScriptRoot/entities.yml"
-)
-
-Write-Verbose 'loading dependencies'
-foreach ($item in (Get-ChildItem -Path "$PSScriptRoot/src/" -Recurse -File))
+Write-Verbose 'Loading tasks'
+foreach ($item in (Get-ChildItem -Filter 'task.ps1' -recurse))
 {
-    Write-Verbose "Loading: $($item.Name)"
-    . $item.FullName
+    Write-Verbose "Found task at: $($item.FullName)"
+    try 
+    {
+        . $item.FullName
+    }
+    catch 
+    {
+        Write-Error -Message "Error on task: $($item.FullName)"
+        $Error[0]
+    }
 }
-
-Write-Verbose "Confirming privileged mode is on."
-if (-not(Confirm-Administrator)) { break; }
-
-Write-Verbose 'Confirming Chocolatey Is installed.'
-if (-not(Confirm-Chocolatey)) { Install-Chocolatey }
-
-Write-Verbose "Loading modules"
-if (-not(Get-Module -ListAvailable 'powershell-yaml'))
-{
-    Install-Module -Name 'powershell-yaml'
-}
-else
-{
-    Import-Module -Name 'powershell-yaml'
-}
-
-Write-Verbose "Loading Scripts"
-$packages = Get-Content -Path $PackagesPath -Raw | ConvertFrom-Yaml
-$packages.packages
-
-foreach ($package in $packages.packages)
-{
-    choco upgrade -y $package
-}
-
-. $PSScriptRoot\src\powershellprofile\task.ps1
